c641cb69b0e205dc49f5adf142d03bca8232141d
Fantop <sethmiller464@yahoo.com>
2017-10-04 19:31:02 -0700

    moved samples to their own object, urls can now contain some parameters

--- main.js
+++ main.js
@@ -1,129 +1,76 @@
+window.onload = function() {
+    var urlParams = {};
+    location.search.replace(
+        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+        function($0, $1, $2, $3) {
+            urlParams[$1] = $3;
+        }
+    );
+
+    if (Object.keys(urlParams).indexOf("word") !== -1) {
+        preserveURL = true;
+        document.getElementById("word").value = urlParams.word;
+        getPhonemes();
+        if (Object.keys(urlParams).indexOf("autoplay") !== -1 && urlParams.autoplay) {
+            readPhonemes();
+        }
+    }
+}
+
 function loadAudio() {
-    AA = new Audio('phonemes/AA.mp3');
-    AA.onended = playNextPhoneme;
+    window.phonemeSamples = {};
 
-    AE = new Audio('phonemes/AE.mp3');
-    AE.onended = playNextPhoneme;
+    phonemeSamples.AA = new Audio('phonemes/AA.mp3');
+    phonemeSamples.AE = new Audio('phonemes/AE.mp3');
+    phonemeSamples.AH = new Audio('phonemes/AH.mp3');
+    phonemeSamples.AO = new Audio('phonemes/AO.mp3');
+    phonemeSamples.AW = new Audio('phonemes/AW.mp3');
+    phonemeSamples.AY = new Audio('phonemes/AY.mp3');
+    phonemeSamples.B = new Audio('phonemes/B.mp3');
+    phonemeSamples.C = new Audio('phonemes/C.mp3');
+    phonemeSamples.CH = new Audio('phonemes/CH.mp3');
+    phonemeSamples.D = new Audio('phonemes/D.mp3');
+    phonemeSamples.DH = new Audio('phonemes/DH.mp3');
+    phonemeSamples.EH = new Audio('phonemes/EH.mp3');
+    phonemeSamples.ER = new Audio('phonemes/ER.mp3');
+    phonemeSamples.EY = new Audio('phonemes/EY.mp3');
+    phonemeSamples.F = new Audio('phonemes/F.mp3');
+    phonemeSamples.G = new Audio('phonemes/G.mp3');
+    phonemeSamples.HH = new Audio('phonemes/HH.mp3');
+    phonemeSamples.IH = new Audio('phonemes/IH.mp3');
+    phonemeSamples.IY = new Audio('phonemes/IY.mp3');
+    phonemeSamples.JH = new Audio('phonemes/JH.mp3');
+    phonemeSamples.K = new Audio('phonemes/K.mp3');
+    phonemeSamples.L = new Audio('phonemes/L.mp3');
+    phonemeSamples.M = new Audio('phonemes/M.mp3');
+    phonemeSamples.N = new Audio('phonemes/N.mp3');
+    phonemeSamples.NG = new Audio('phonemes/NG.mp3');
+    phonemeSamples.OW = new Audio('phonemes/OW.mp3');
+    phonemeSamples.OY = new Audio('phonemes/OY.mp3');
+    phonemeSamples.P = new Audio('phonemes/P.mp3');
+    phonemeSamples.R = new Audio('phonemes/R.mp3');
+    phonemeSamples.S = new Audio('phonemes/S.mp3');
+    phonemeSamples.SH = new Audio('phonemes/SH.mp3');
+    phonemeSamples.T = new Audio('phonemes/T.mp3');
+    phonemeSamples.TH = new Audio('phonemes/TH.mp3');
+    phonemeSamples.UH = new Audio('phonemes/UH.mp3');
+    phonemeSamples.UW = new Audio('phonemes/UW.mp3');
+    phonemeSamples.V = new Audio('phonemes/V.mp3');
+    phonemeSamples.W = new Audio('phonemes/W.mp3');
+    phonemeSamples.Y = new Audio('phonemes/Y.mp3');
+    phonemeSamples.Z = new Audio('phonemes/Z.mp3');
+    phonemeSamples.ZH = new Audio('phonemes/ZH.mp3');
 
-    AH = new Audio('phonemes/AH.mp3');
-    AH.onended = playNextPhoneme;
-
-    AO = new Audio('phonemes/AO.mp3');
-    AO.onended = playNextPhoneme;
-
-    AW = new Audio('phonemes/AW.mp3');
-    AW.onended = playNextPhoneme;
-
-    AY = new Audio('phonemes/AY.mp3');
-    AY.onended = playNextPhoneme;
-
-    B = new Audio('phonemes/B.mp3');
-    B.onended = playNextPhoneme;
-
-    C = new Audio('phonemes/C.mp3');
-    C.onended = playNextPhoneme;
-
-    CH = new Audio('phonemes/CH.mp3');
-    CH.onended = playNextPhoneme;
-
-    D = new Audio('phonemes/D.mp3');
-    D.onended = playNextPhoneme;
-
-    DH = new Audio('phonemes/DH.mp3');
-    DH.onended = playNextPhoneme;
-
-    EH = new Audio('phonemes/EH.mp3');
-    EH.onended = playNextPhoneme;
-
-    ER = new Audio('phonemes/ER.mp3');
-    ER.onended = playNextPhoneme;
-
-    EY = new Audio('phonemes/EY.mp3');
-    EY.onended = playNextPhoneme;
-
-    F = new Audio('phonemes/F.mp3');
-    F.onended = playNextPhoneme;
-
-    G = new Audio('phonemes/G.mp3');
-    G.onended = playNextPhoneme;
-
-    HH = new Audio('phonemes/HH.mp3');
-    HH.onended = playNextPhoneme;
-
-    IH = new Audio('phonemes/IH.mp3');
-    IH.onended = playNextPhoneme;
-
-    IY = new Audio('phonemes/IY.mp3');
-    IY.onended = playNextPhoneme;
-
-    JH = new Audio('phonemes/JH.mp3');
-    JH.onended = playNextPhoneme;
-
-    K = new Audio('phonemes/K.mp3');
-    K.onended = playNextPhoneme;
-
-    L = new Audio('phonemes/L.mp3');
-    L.onended = playNextPhoneme;
-
-    M = new Audio('phonemes/M.mp3');
-    M.onended = playNextPhoneme;
-
-    N = new Audio('phonemes/N.mp3');
-    N.onended = playNextPhoneme;
-
-    NG = new Audio('phonemes/NG.mp3');
-    NG.onended = playNextPhoneme;
-
-    OW = new Audio('phonemes/OW.mp3');
-    OW.onended = playNextPhoneme;
-
-    OY = new Audio('phonemes/OY.mp3');
-    OY.onended = playNextPhoneme;
-
-    P = new Audio('phonemes/P.mp3');
-    P.onended = playNextPhoneme;
-
-    R = new Audio('phonemes/R.mp3');
-    R.onended = playNextPhoneme;
-
-    S = new Audio('phonemes/S.mp3');
-    S.onended = playNextPhoneme;
-
-    SH = new Audio('phonemes/SH.mp3');
-    SH.onended = playNextPhoneme;
-
-    T = new Audio('phonemes/T.mp3');
-    T.onended = playNextPhoneme;
-
-    TH = new Audio('phonemes/TH.mp3');
-    TH.onended = playNextPhoneme;
-
-    UH = new Audio('phonemes/UH.mp3');
-    UH.onended = playNextPhoneme;
-
-    UW = new Audio('phonemes/UW.mp3');
-    UW.onended = playNextPhoneme;
-
-    V = new Audio('phonemes/V.mp3');
-    V.onended = playNextPhoneme;
-
-    W = new Audio('phonemes/W.mp3');
-    W.onended = playNextPhoneme;
-
-    Y = new Audio('phonemes/Y.mp3');
-    Y.onended = playNextPhoneme;
-
-    Z = new Audio('phonemes/Z.mp3');
-    Z.onended = playNextPhoneme;
-
-    ZH = new Audio('phonemes/ZH.mp3');
-    ZH.onended = playNextPhoneme;
+    Object.keys(phonemeSamples).forEach(function(key) {
+        phonemeSamples[key].onended = playNextPhoneme;
+    });
 }
 
 loadAudio();
 
 var pronounceButtonDisabled = true;
 var readButtonDisabled = true;
+var preserveURL = false;
 
 function updateButton() {
     if (pronounceButtonDisabled) {
@@ -145,6 +92,10 @@ function oneWord(evt) {
 }
 
 function getPhonemes() {
+    if (!preserveURL) {
+        window.history.replaceState(null, null, '?word=' + document.getElementById("word").value);
+    }
+    preserveURL = false;
     document.getElementById("word").value = document.getElementById("word").value.replace(/[^a-z]/gi, '').toLowerCase();
 
     word = document.getElementById("word").value;
@@ -172,7 +123,7 @@ function readPhonemes() {
 
 function playNextPhoneme() {
     if (i < phonemeList.length) {
-        window[phonemeList[i]].play();
+        phonemeSamples[phonemeList[i]].play();
         document.getElementsByClassName("phonemeParts")[i].style.color = "#FF4081";
     }
     if (i > 0) {
